bccf13bdf99124645d8848cef1b98cbcb282a1e0
fakhreddine <largofakhrddin@gmail.com>
2025-04-08 13:17:47 +0100

    add logic in js for validation input

--- main.js
+++ main.js
@@ -7,34 +7,90 @@ const inputAmount = document.getElementById("mortgage-amount");
 const inputTerm = document.getElementById("mortgage-term");
 const inputRate = document.getElementById("mortgage-rate");
 
-const input = document.querySelectorAll("input");
+const divAmount = document.querySelector(".mortgage-amount");
+const divTerm = document.querySelector(".mortgage-term");
+const divRate = document.querySelector(".mortgage-rate");
+const mortgageYrs = document.querySelector(".mortgage-yrs");
+const mortgagePsnt = document.querySelector(".mortgage-psnt");
+const amountMon = document.querySelector(".amount-mon");
+
+
+const error1 = document.querySelector(".error1")
+const error2 = document.querySelector(".error2")
+const error3 = document.querySelector(".error3")
+
+
+function amountValide() {
+  let inputAmountValue = parseFloat(inputAmount.value);
+  
+  if (inputAmountValue >= 1 && inputAmountValue <= 1000000) {
+    divAmount.style.backgroundColor = "hsl(202, 86%, 94%)";
+    divAmount.style.border = "solid hsl(200, 24%, 40%) .15rem";
+    amountMon.style.color = "hsl(200, 24%, 40%)";
+    error1.classList.add('hidden')
+    return true;
+  } else {
+    divAmount.style.backgroundColor = "hsl(4, 69%, 50%)";
+    divAmount.style.border = "solid hsl(4, 69%, 50%) .15rem";
+    amountMon.style.color = "white";
+    error1.classList.remove('hidden')
+    return false;
+  }
+}
+
+function termValide(){
+
+
+   let inputTermValue = parseFloat(inputTerm.value);
+
+if (inputTermValue >= 1 && inputTermValue <= 50) {
+  divTerm.style.backgroundColor = "hsl(202, 86%, 94%)";
+  divTerm.style.border = " solid  hsl(200, 24%, 40%)  .15rem";
+  mortgageYrs.style.color = "hsl(200, 24%, 40%)";
+  error2.classList.add('hidden')
+  return true
+} else {
+  divTerm.style.backgroundColor = "hsl(4, 69%, 50%)";
+  divTerm.style.border = " solid  hsl(4, 69%, 50%)  .15rem";
+  mortgageYrs.style.color = "white";
+  console.log("not");
+  error2.classList.remove('hidden')
+  return false
+}
 
 
 
+}
 
-check.forEach((Element) => {
-  Element.addEventListener('click', () => {
-    const changeColor = document.querySelectorAll(".changeColor");
-    
-    // Add the class to each element in the NodeList
-    for (let i = 0; i < changeColor.length; i++) {
-      changeColor[i].classList.add("changeColorBlue");
-    }
-    
-    // Remove the class from all elements after the click event
-    // changeColor.forEach((item) => {
-    //   item.classList.remove("changeColorBlue");
-    // });
-  });
-});
 
+
+function rateValide(){
+
+
+  let inputRateValue = parseFloat(inputRate.value);
+
+  if (inputRateValue > 0 && inputRateValue <= 100) {
+    divRate.style.backgroundColor = "hsl(202, 86%, 94%)";
+    divRate.style.border = " solid  hsl(200, 24%, 40%)  .15rem";
+    mortgagePsnt.style.color = "hsl(200, 24%, 40%)";
+    error3.classList.add('hidden')
+    return true
+  } else {
+    divRate.style.backgroundColor = "hsl(4, 69%, 50%)";
+    divRate.style.border = " solid hsl(4, 69%, 50%) .15rem";
+    mortgagePsnt.style.color = "white";
+    error3.classList.remove('hidden')
+    return false
+  }
+
+}
 
 
 
 function addButton() {
-  var inputAmountValue = inputAmount.value;
-  var inputTermValue = inputTerm.value;
-  var inputRateValue = inputRate.value;
+  let inputAmountValue = inputAmount.value;
+  let inputTermValue = inputTerm.value;
+  let inputRateValue = inputRate.value;
 
   if (
     inputAmountValue >= 1 &&
@@ -67,13 +123,30 @@ function addButton() {
       
       `;
   } else {
-    input.forEach((inputElement) => {
-      inputElement.classList.add("if-not");
-    });
+    console.log("hello");
+    
+   amountValide() 
+    termValide()
+    rateValide()
   }
 }
 
-// const queries = new URLSearchParams(window.location.search);
-// const selectedTerm = queries.get("term");
-// const selectedRate = queries.get("rate");
-// const selectedAmount = queries.get("amount");
+
+function clearAll(){
+
+   inputAmount.value = "e"
+    inputTerm.value = "e"
+   inputRate.value = "e"
+
+   console.log(inputAmount,inputTerm,inputRate)
+
+   secondryCard.innerHTML = `
+   
+    <img class="illustration-img" src="/assets/images/illustration-empty.svg" alt="illustration">
+          <span class="result">Results shown here</span>
+          <p class="paragraph">
+            Complete the form and click "calculate repayments" to see what your monthly repayments would be.
+          </p> 
+   `
+
+}
